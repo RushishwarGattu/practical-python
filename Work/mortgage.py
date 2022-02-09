a8a88ef0bb04edb59b271bb40fb6765559dd2f90
RushishwarGattu <rushishwar.gattu@tigeranalytics.com>
2022-02-09 15:28:29 +0530

    commited changes

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,25 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+i=0
+
+while principal > 0:
+
+    
+    if i>=61 and i<=108:
+        principal = principal * (1+rate/12) - payment-1000
+        total_paid = total_paid + payment+1000
+    else:
+        principal = principal * (1+rate/12) - payment
+    
+        total_paid = total_paid + payment
+    i+=1
+
+    print(i, total_paid,principal)
